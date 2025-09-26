26e5ccc2f4716f0d310091a9dbcbc5b310d4c4ac
heyiamdavid522 <heyiamdavid19@gmail.com>
2025-09-25 19:19:48 -0500

    feat: actualizacion de Qr

--- qr_code/generator_qr_code.py
+++ qr_code/generator_qr_code.py
@@ -1,13 +1,27 @@
 import qrcode
+from PIL import Image, ImageDraw
 
-texto = ""
-qr = qrcode.QRCode(version=1,
-                   box_size=10,
-                   border=2)
-
+texto = "https://github.com/heyiamdavid/web_empresa"
+qr = qrcode.QRCode(
+    version=6,  
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=2
+)
 qr.add_data(texto)
 qr.make(fit=True)
+qr_img = qr.make_image(fill_color="black", back_color="white").convert("RGB")
+logo = Image.open("assets/images/prueba.png")
+qr_width, qr_height = qr_img.size
+logo_size = qr_width // 5
+logo = logo.resize((logo_size, logo_size), Image.LANCZOS)
+logo_width, logo_height = logo.size
+pos = ((qr_width - logo_width) // 2, (qr_height - logo_height) // 2)
+draw = ImageDraw.Draw(qr_img)
+draw.rectangle(
+    [pos, (pos[0] + logo_width, pos[1] + logo_height)],
+    fill="white"
+)
+qr_img.paste(logo, pos, mask=logo if logo.mode == "RGBA" else None)
+qr_img.save("assets/qr_image/qr_logo2.png")
 
-import_qr = qr.make_image(fill_color="black", 
-                          back_color="white")
-import_qr.save("assets/qr_image/qr_image.png")
